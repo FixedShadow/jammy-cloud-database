98a7997f2190d1ad11d96dc639679182f3e217e6
larosa <11344043+larosa123@user.noreply.gitee.com>
2025-04-27 23:43:23 +0800

    first commit

---
+++ mysql-instance-management/cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "mysql-instance-management",
+	Short: "mysql-instance-management",
+	Run: func(cmd *cobra.Command, args []string) {
+		return
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ mysql-instance-management/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/FixedShadow/jammy-cloud-database/mysql-instance-management/cmd"
+
+func main() {
+	cmd.Execute()
+}
