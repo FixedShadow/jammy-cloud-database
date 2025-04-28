d064d7133d94b75b18aee2ff61cc011806cac077
larosa <11344043+larosa123@user.noreply.gitee.com>
2025-04-28 13:54:15 +0800

    add example code

---
+++ example/go.mod
@@ -0,0 +1,33 @@
+module github.com/FixedShadow/jammy-cloud-database/example
+
+go 1.23.8
+
+require github.com/canonical/lxd v0.0.0-20250426204749-b8840644509c
+
+require (
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.5 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/pkg/sftp v1.13.9 // indirect
+	github.com/pkg/xattr v0.4.10 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/zitadel/logging v0.6.2 // indirect
+	github.com/zitadel/oidc/v3 v3.37.0 // indirect
+	github.com/zitadel/schema v1.3.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/otel v1.35.0 // indirect
+	go.opentelemetry.io/otel/metric v1.35.0 // indirect
+	go.opentelemetry.io/otel/trace v1.35.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/net v0.39.0 // indirect
+	golang.org/x/oauth2 v0.29.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)
---
+++ example/how_to.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	lxd "github.com/canonical/lxd/client"
+	"github.com/canonical/lxd/shared/api"
+	"os"
+)
+
+/**
+this file show how to execute the lxd operation by go sdk.
+https://pkg.go.dev/github.com/lxc/lxd/client
+*/
+
+var param = lxd.ConnectionArgs{InsecureSkipVerify: true}
+
+func main() {
+	InitBasicParam()
+	CreateVmInstance()
+}
+
+func InitBasicParam() {
+	certContent, err := os.ReadFile("my.crt")
+	if err != nil {
+		panic(err)
+	}
+	keyContent, err := os.ReadFile("my.key")
+	if err != nil {
+		panic(err)
+	}
+	param.TLSClientCert = string(certContent)
+	param.TLSClientKey = string(keyContent)
+}
+
+func CreateVmInstance() {
+	instanceServer, err := lxd.ConnectLXD("https://x.x.x.x:8443", &param)
+	if err != nil {
+		panic(err)
+	}
+	instance := api.InstancesPost{}
+	instance.Name = "my-vm-001"
+	instance.Type = "virtual-machine"
+	instance.Devices = map[string]map[string]string{
+		"root": {
+			"path": "/",
+			"pool": "default",
+			"size": "12GiB",
+			"type": "disk",
+		},
+	}
+	instance.Config = map[string]string{
+		"limits.cpu":    "1",
+		"limits.memory": "2GiB",
+	}
+	instance.Source = api.InstanceSource{
+		Type:        "image",
+		BaseImage:   "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", //image fingerprint
+		Fingerprint: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", //image fingerprint
+	}
+	op, err := instanceServer.CreateInstance(instance)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("operation id: ", op.Get().ID)
+	fmt.Println("operation status code: ", op.Get().StatusCode.String())
+	fmt.Println("operation err: ", op.Get().Err)
+
+	err = op.Wait() //wait for the operation to complete
+	if err != nil {
+		panic(err)
+	}
+	state := api.InstanceStatePut{
+		Action:  "start",
+		Timeout: -1,
+	}
+	op, err = instanceServer.UpdateInstanceState(instance.Name, state, "")
+	if err != nil {
+		panic(err)
+	}
+	err = op.Wait() //wait for the operation to complete.
+	if err != nil {
+		panic(err)
+	}
+}
