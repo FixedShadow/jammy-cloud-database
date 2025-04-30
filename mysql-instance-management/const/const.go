2dfc720545ee3207a544c72bda139f56c06d3e79
larosa <11344043+larosa123@user.noreply.gitee.com>
2025-04-30 22:29:08 +0800

    add specs

---
+++ mysql-instance-management/const/const.go
@@ -0,0 +1,5 @@
+package constant
+
+const MYSQL_SIZE_S1_MICRO = "db.mysql.s1.micro"
+
+const MYSQL_SIZE_S1_SMALL = "db.mysql.s1.small"
--- mysql-instance-management/handler/instance_management_service.go
+++ mysql-instance-management/handler/instance_management_service.go
@@ -1,11 +1,17 @@
 package handler
 
-import "context"
+import (
+	"context"
+	constant "github.com/FixedShadow/jammy-cloud-database/mysql-instance-management/const"
+)
 import pb "github.com/FixedShadow/jammy-cloud-database/mysql-instance-management/proto"
 
 type InstanceManagementService struct{}
 
 func (s *InstanceManagementService) CreateDBInstance(ctx context.Context, req *pb.CreateDBInstanceRequest) (res *pb.CreateDBInstanceResponse, err error) {
-
+	switch req.InstanceClass {
+	case constant.MYSQL_SIZE_S1_MICRO:
+	case constant.MYSQL_SIZE_S1_SMALL:
+	}
 	return res, nil
 }
