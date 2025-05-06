c299e351646f3fef6a521a27f6dac833229c2c23
larosa <11344043+larosa123@user.noreply.gitee.com>
2025-05-06 22:03:50 +0800

    modify install script

--- mysql-monitor-agent/script/install_mysql.sh
+++ mysql-monitor-agent/script/install_mysql.sh
@@ -5,13 +5,28 @@ echo "installing mysql5.7"
 export LANG=en_US.UTF-8
 export LANGUAGE=en_US:
 
+
 TAR_FILE=/opt/mysql-5.7.20-linux-glibc2.12-x86_64.tar.gz
 WD_HOME=/opt
 MYSQL_DIR=/opt/mysql
 MYSQL_DATA_DIR=/opt/mysql/data
 
+if [ -d ${MYSQL_DIR} ]; then
+  echo "MySQL is already installed."
+  echo "Now check whether MySQL has been started"
+  mysql_process=$(ps -ef|grep "/opt/mysql/bin/mysqld" |grep -v grep)
+  if [ -n "$mysql_process" ]; then
+    echo "mysql already has been started"
+    exit 0
+  else
+    /opt/mysql/support-files/mysql.server start
+  fi
+fi
+
 
 tar -zxvf ${TAR_FILE} -C ${WD_HOME}
+
+rm -rf ${TAR_FILE}
 
 mv /${WD_HOME}/mysql-5.7.20-linux-glibc2.12-x86_64 ${MYSQL_DIR}
 
@@ -40,8 +55,6 @@ ${MYSQL_DIR}/bin/mysql_ssl_rsa_setup  --datadir=${MYSQL_DATA_DIR}
 cat ${MYSQL_DIR}/logs/error.log |grep "temporary password" |awk '{print $NF}' >> ${MYSQL_DIR}/tmp_password.txt
 
 chown -R mysql:mysql ${MYSQL_DIR}
-
-
 
 
 
